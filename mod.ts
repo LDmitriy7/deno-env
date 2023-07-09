5ffc46a9e97f30353cc3e524d2f8b7cac1b0372e
Dmitriy <levchenko.d.a1998@gmail.com>
2023-07-10 02:09:01 +0600

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -3,14 +3,19 @@ import "https://deno.land/std@0.182.0/dotenv/load.ts"
 class Env {
   str(key: string) {
     const value = Deno.env.get(key)
-    if (!value) throw new EnvError(key)
+    if (!value) throw new EnvError(key, "not set")
     return value
+  }
+  int(key: string) {
+    const value = this.str(key)
+    if (!Number.isInteger(value)) throw new EnvError(key, "not int")
+    return Number(value)
   }
 }
 
 class EnvError extends Error {
-  constructor(key: string) {
-    super(`You must set "${key}" environment variable`)
+  constructor(key: string, description: string) {
+    super(`Environment variable "${key}" error: ${description}`)
   }
 }
 
